7f59e5c3344a4ca5921a166a9057ff5bf5e8e530
capybara120404 <D.L.Borodin@outlook.com>
2024-10-25 20:34:26 +0300

    A model for describing TV series has been created.

---
+++ models/series.go
@@ -0,0 +1,17 @@
+package models
+
+type SeriesInfo struct {
+	ID            int
+	Title         string
+	Link          string
+	IMDB          string
+	StartYear     string
+	EndYear       string
+	Poster        string
+	OriginalTitle string
+	Country       string
+	Genre         string
+	Year          string
+	Status        string
+	NumberOfViews uint
+}
