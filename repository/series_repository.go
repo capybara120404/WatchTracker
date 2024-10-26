16a5e009b9e83412d8f2c74c2b76da796edb734f
capybara120404 <D.L.Borodin@outlook.com>
2024-10-27 01:35:22 +0300

    Added functionality to get all shows by id as well.

--- repository/series_repository.go
+++ repository/series_repository.go
@@ -18,7 +18,7 @@ func NewSeriesInfoRepository(connecter *database.Connecter) *SeriesInfoRepositor
 	}
 }
 
-func (repository *SeriesInfoRepository) Add(series models.SeriesInfo) (int64, error) {
+func (repository *SeriesInfoRepository) Add(series models.Series) (int64, error) {
 	res, err := repository.db.Exec("INSERT INTO series (title, link, imdb, start_year, end_year, poster, country) VALUES (:title, :link, :imdb, :start_year, :end_year, :poster, :country)",
 		sql.Named("title", series.Title),
 		sql.Named("link", series.Link),
@@ -37,4 +37,61 @@ func (repository *SeriesInfoRepository) Add(series models.SeriesInfo) (int64, er
 	}
 
 	return id, nil
+}
+
+func (repository *SeriesInfoRepository) GetAll() ([]models.Series, error) {
+	rows, err := repository.db.Query("SELECT * FROM series ORDER BY title LIMIT 10")
+	if err != nil {
+		return nil, fmt.Errorf("error querying series from the database")
+	}
+	defer rows.Close()
+
+	var series []models.Series
+	for rows.Next() {
+		var s models.Series
+
+		err := rows.Scan(&s.ID,
+			&s.Title,
+			&s.Link,
+			&s.IMDB,
+			&s.StartYear,
+			&s.EndYear,
+			&s.Poster,
+			&s.Country)
+		if err != nil {
+			return nil, fmt.Errorf("error scanning series data")
+		}
+
+		series = append(series, s)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("error iterating over series rows")
+	}
+
+	return series, nil
+}
+
+func (repository *SeriesInfoRepository) GetById(id int) (models.Series, error) {
+	row := repository.db.QueryRow("SELECT * FROM series WHERE id = :id",
+		sql.Named("id", id))
+	var series models.Series
+
+	err := row.Scan(&series.ID,
+		&series.Title,
+		&series.Link,
+		&series.IMDB,
+		&series.StartYear,
+		&series.EndYear,
+		&series.Poster,
+		&series.Country)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return series, fmt.Errorf("series not found")
+		} else {
+			return series, fmt.Errorf("error retrieving series from database")
+		}
+	}
+
+	return series, nil
 }
